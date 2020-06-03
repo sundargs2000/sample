21da846e5ff4d95a3f7918d54ce2be25d377048f
tsugunt <66036707+tsugunt@users.noreply.github.com>
2020-06-04 00:11:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var app = express();
 const session = require('express-session');
 var mongoose = require('mongoose');
 
-
 //local strategy
 var register = require('./controllers/config/auth');
 var LocalStrategy = require('passport-local').Strategy;
